6233408f9269a8ee8b1eaf114acbcdda1b8f4967
Shorya Tyagi <shoryatyagi29@gmail.com>
2023-05-01 05:10:32 +0530

    basic_questions

---
+++ Day2/reverse_a_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter a number : ";
+    cin>>num;
+
+    int result=0 ;
+    while(num!=0){
+        int reminder = num%10;
+        result = result*10 + reminder;
+        num=num/10;
+
+    }
+    cout<< "After reversing : "<<result;
+
+
+}
