d5cb03560a3998887e04b893deb42e82769d8c9e
Shorya Tyagi <shoryatyagi29@gmail.com>
2023-10-15 21:20:19 +0530

    min element in array

--- Day 16/min_element_array.cpp
+++ Day 16/min_element_array.cpp
@@ -6,12 +6,13 @@ int main()
 {
     int arr[5] = {22, 45, 67, 18, 6};
     int min = INT_MAX;
-
+    int size = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < 5; i++)
     {
         if (arr[i] < min)
             min = arr[i];
     }
+    cout << "Size : " << size << endl;
     cout << "Min : " << min << endl;
 
     return 0;
